54a1fcb174b11643f199b48ea3ca45f448c035a2
EmilioVS <6360@holbertonstudents.com>
2023-06-12 13:19:57 -0700

    proyecto 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-
-
-
 if __name__ == "__main__":
-    
-    """Print the addition of all arguments."""
-    
     import sys
-    
-
-    
-    total = 0
-    
-    for i in range(len(sys.argv) - 1):
-        
-        total += int(sys.argv[i + 1])
-        
-    print("{}".format(total))
-    
+    argv = sys.argv[1:]
+    arg = len(argv)
+    if (arg == 0):
+        print("0")
+    else:
+        c = 0
+        for a in range(arg):
+            c += int(argv[a])
+        print("{}".format(c))
