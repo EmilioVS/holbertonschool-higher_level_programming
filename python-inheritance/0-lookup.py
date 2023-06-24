b652246c573f15e260281d1cab8205e1efa1f3fc
EmilioVS <6360@holbertonstudents.com>
2023-06-24 16:34:21 -0700

    EJERCICIO 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 15:13:37 2020
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
